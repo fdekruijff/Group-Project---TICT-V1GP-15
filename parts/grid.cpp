2c456109e80912ddddd5ed2c82c4c4b60a89fcbb
SFenijn <stijn.fenijn@student.hu.nl>
2018-03-29 15:34:36 +0200

    grid tst

--- parts/grid.cpp
+++ parts/grid.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-#include "BrickPi3.h"
+#include "BrickPi3.h"   // for BrickPi3
+#include <iostream>     // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+#include <vector>       // for dynamic array
 
 using namespace std;
 
-int main()
+BrickPi3 BP;
 
+void exit_signal_handler(int signo);
+
+//Stop
+void stop(void)
+	 BP.set_motor_power(PORT_A, 0);
+	 BP.set_motor_power(PORT_B, 0);
+	 BP.set_motor_power(PORT_C, 0);
