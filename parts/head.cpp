e293a9c83bda477033c194e42497c6389ec68b06
Joepieler <be.campagne@live.nl>
2018-04-03 11:03:55 +0200

    Update head.cpp

--- parts/head.cpp
+++ parts/head.cpp
@@ -1,64 +1,67 @@
 #include <cmath>
+#include <cmath>
 #include <iostream>
-#include <thread>
-#include "./piprograms/BrickPi3.cpp"
-#include <vector>
 #include <unistd.h>
-#include <algorithm>
-#include <signal.h>     
+#include "./piprograms/BrickPi3.cpp"
+#include <thread>
+#include <vector>
 
 using namespace std;
 
+
 BrickPi3 BP;
+uint8_t s_contrast = PORT_2;            // Light sensor
+uint8_t m_head = PORT_A;                // Head motor
+uint8_t m_left = PORT_B;                // Left motor
+uint8_t m_right = PORT_C;               // Right motor
 
-void exit_signal_handler(int signo);
+//Exit signal
 
-void stop(void){
-	BP.set_motor_power(PORT_A, 0);
-	//BP.set_motor_power(PORT_B, 0);
-	//BP.set_motor_power(PORT_C, 0);
-}
+
+//void exit_signal_handler(int signo);
 
 void brick_py_setup() {
-	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
-	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
-	BP.set_motor_limits(PORT_A, 40, 0);
+	//signal(SIGINT, exit_signal_handler);
+	BP.detect();
+	BP.set_sensor_type(s_contrast, SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_ULTRASONIC); //Ultrasonic sensor setup
+}
+
+void stopHead(void){
+	//Useless function
+	BP.set_motor_power(m_right, -128);
 }
 
 
 int turnHead(int degree){
-    BP.set_motor_position(PORT_A, degree);
-    return 1;
+	BP.set_motor_position(PORT_A, degree);
+	return 1;
 }
 
-int head(int degrees){
-    turnHead(degrees);
-  signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
-  BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
-  int error;
-  BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_ULTRASONIC);
-  sensor_ultrasonic_t Ultrasonic2;
-for (int i =0; i < 1000;i++){
-    if(BP.get_sensor(PORT_3, Ultrasonic2) == 0){
-		//cout << "Ultrasonic sensor (S2): "   << Ultrasonic2.cm << "cm" << endl;
-		//usleep(100);
- 	}
+int head(void){
+	sensor_ultrasonic_t Ultrasonic2;
+	
+	for (int i =0; i<1000 ;i++){
+		BP.get_sensor(PORT_3, Ultrasonic2); //Check 1000 times Ultrasonic sensor to get data
+	}
+	return Ultrasonic2.cm;
 }
-turnHead(degrees*-1);
-cout << Ultrasonic2.cm<< endl;
+
+int turnHeadScan(int degree){
+	turnHead(degree);
+	sleep(0.5);
+	int dist = head();
+	sleep(0.5);
+	turnHead(degree*-1);
+	return dist;
 }
 
 
 int main(){
 	brick_py_setup();
-    head(50);
+	cout << turnHeadScan(50)<< endl;
+	stopHead();
 }
 
 
-// Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo){
-  if(signo == SIGINT){
-    BP.reset_all();    // Reset everything so there are no run-away motors
-    exit(-2);
-  }
-}
+
