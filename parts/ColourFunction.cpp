7c2d3a47019b6553e4aa92dc4cda2a456f27136b
SFenijn <stijn.fenijn@student.hu.nl>
2018-04-03 13:34:03 +0200

    ColourValue

--- parts/ColourFunction.cpp
+++ parts/ColourFunction.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 BrickPi3 BP;
-uint8_t s_colour = PORT_1;				// Colour sensor
+uint8_t s_color = PORT_1;				// Colour sensor
 uint8_t s_contrast = PORT_2;            // Light sensor
 uint8_t m_head = PORT_A;                // Head motor
 uint8_t m_left = PORT_B;                // Left motor
@@ -17,27 +17,22 @@ uint8_t m_right = PORT_C;               // Right motor
 
 bool Calibrate = false;
 
-int16_t Colour_Value = 0;
+int16_t Color_Value = 0;
 
 void Setup()
 {
 	BP.detect();
 	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
+	sensor_color_t      Color1;
 }
 
 bool Find_Red(void)
 {
-	if(BP.get_sensor(PORT_1, Color1) == )
+	cout << BP.get_sensor(PORT_1, Color1);
 }
 
 int main()
 {
 	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
 	
-	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
-	
-	int error;
-	
-	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
-	sensor_color_t      Color1;
 }
